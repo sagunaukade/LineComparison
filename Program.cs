7adc50225215c0246fd7e2b8de4b529b76907db1
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-23 23:39:48 +0530

    Adding First Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fellowship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to linecomparision problems");
+            Line_comparision Linecom = new Line_comparision();
+            Linecom.length_line();
+            Linecom.Comparelines();
+            Console.ReadLine();
+        }
+    }
+}
